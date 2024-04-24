ae3aa318c3e8f2805950a786a41a71d076a4ea9b
KarimYasser15 <karimyasser1510@gmail.com>
2024-04-25 00:28:07 +0200

    Timer Bug
    Fixed Bug when timer ends the lesson isn't finished

--- src/jmemorize/gui/swing/panels/TimerPanel.java
+++ src/jmemorize/gui/swing/panels/TimerPanel.java
@@ -1,7 +1,7 @@
 /*
  * jMemorize - Learning made easy (and fun) - A Leitner flashcards tool
  * Copyright(C) 2004-2006 Riad Djemili
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 1, or (at your option)
@@ -23,13 +23,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.JTextField;
-import javax.swing.Timer;
+import javax.swing.*;
 
+import jmemorize.core.learn.DefaultLearnSession;
 import jmemorize.core.learn.LearnSession;
+import jmemorize.gui.LC;
 import jmemorize.gui.Localization;
+import jmemorize.gui.swing.actions.AbstractAction2;
+import jmemorize.gui.swing.actions.LearnAction;
 
 /**
  * @author djemili
@@ -49,6 +50,24 @@ public class TimerPanel extends JPanel implements ActionListener
 
     private LearnSession  m_learnSession;
 
+    private LearnPanel m_learnPanel;
+    JButton stopLearningButton = new JButton(new StopAction());
+
+
+    private class StopAction extends AbstractAction2
+    {
+        public StopAction()
+        {
+            setName(Localization.get(LC.LEARN_STOP));
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            m_learnSession.endLearning();
+        }
+    }
+
+
     public void start(LearnSession learnSession, int seconds)
     {
         m_learnSession = learnSession;
@@ -57,7 +76,7 @@ public class TimerPanel extends JPanel implements ActionListener
 
         removeAll();
         setLayout(new BorderLayout());
-        
+
         // if there is a time limit we show a progess bar
         // otherwise only a label
         if (m_secondsTarget > -1)
@@ -74,12 +93,12 @@ public class TimerPanel extends JPanel implements ActionListener
             m_textField.setEditable(false);
             add(m_textField, BorderLayout.CENTER);
         }
-        
+
         // call action performer every second
         m_timer = new Timer(1000, this);
         m_timer.start();
     }
-    
+
     public void start(LearnSession strategy)
     {
         start(strategy, -1);
@@ -109,10 +128,13 @@ public class TimerPanel extends JPanel implements ActionListener
         {
             m_textField.setText(getTimeString());
         }
-        
+
         if (m_secondsPassed == m_secondsTarget)
         {
+
             m_timer.stop();
+            m_learnSession.endLearning();
+            stopLearningButton.setEnabled(true);
             m_learnSession.onTimer();
         }
     }
@@ -129,30 +151,30 @@ public class TimerPanel extends JPanel implements ActionListener
             if (m_secondsTarget <= m_secondsPassed)
             {
                 return timeString(m_secondsTarget, m_secondsTarget) + timeExtString(m_secondsTarget)
-                     + " " + Localization.get("Time.PASSED") + "!"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                        + " " + Localization.get("Time.PASSED") + "!"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
             }
             else
             {
                 return timeString(m_secondsPassed, m_secondsTarget) + " / " + //$NON-NLS-1$
-                    timeString(m_secondsTarget, m_secondsTarget) + timeExtString(m_secondsTarget);
+                        timeString(m_secondsTarget, m_secondsTarget) + timeExtString(m_secondsTarget);
             }
         }
         // else show just a text label
         else
         {
             return timeString(m_secondsPassed, m_secondsPassed) +
-                timeExtString(m_secondsPassed);
+                    timeExtString(m_secondsPassed);
         }
     }
-    
+
     protected String timeString(int seconds, int secondsTarget)
     {
         if (secondsTarget > 60*60) // show hours if over 60 minutes
         {
             return (seconds/(60*60)) +":"+ m_formater.format((seconds/60)%60)  //$NON-NLS-1$
-                +":"+ m_formater.format(seconds%60); //$NON-NLS-1$
+                    +":"+ m_formater.format(seconds%60); //$NON-NLS-1$
         }
-        
+
         if (secondsTarget > 60) // show minutes if over 60 seconds
         {
             return (seconds/60) +":"+ m_formater.format(seconds%60); //$NON-NLS-1$
@@ -160,19 +182,19 @@ public class TimerPanel extends JPanel implements ActionListener
 
         return Integer.toString(seconds);
     }
-    
+
     protected String timeExtString(int seconds)
     {
         if (seconds > 60*60)
         {
             return " "+ Localization.get("Time.HOURS");    //$NON-NLS-1$ //$NON-NLS-2$
         }
-        
+
         if (seconds > 60)
         {
             return " " + Localization.get("Time.MINUTES"); //$NON-NLS-1$ //$NON-NLS-2$
         }
-            
+
         return " " + Localization.get("Time.SECONDS");     //$NON-NLS-1$ //$NON-NLS-2$
     }
 }
